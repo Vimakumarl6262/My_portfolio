df83f5fc4bf37b7c7a53c7ded0cc0db4828419f2
vimalkumar6262 <vk7889032@gmail.com>
2025-07-26 12:33:32 +0530

    Updated portfolio with App Engine deployment changes and gallery enhancements

--- src/main/java/com/vimal/vimalportfolio/HomeController.java
+++ src/main/java/com/vimal/vimalportfolio/HomeController.java
@@ -1,16 +1,16 @@
-// src/main/java/com/vimal/vimalportfolio/HomeController.java
-package com.vimal.vimalportfolio;
+// src/main/java/com/vimal/vimalportfolio/controller/HomeController.java
+package com.vimal.vimalportfolio; // Make sure 'controller' package exists
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // This annotation marks it as a Spring MVC controller
+@Controller // This marks it as a Spring MVC controller
 public class HomeController {
 
     @GetMapping("/") // This maps HTTP GET requests to the root URL ("/")
     public String home() {
         // This tells Spring Boot to look for a template named "index"
-        // in src/main/resources/templates/ and render it.
+        // in src/main/resources/templates/ and render it using Thymeleaf.
         return "index";
     }
 }
